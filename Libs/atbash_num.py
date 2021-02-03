76f8d8cbb674f6fd12969bfc022f27c4f4da40c9
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-02-03 16:22:15 +0100

    Update atbash_num.py

--- Libs/atbash_num.py
+++ Libs/atbash_num.py
@@ -7,5 +7,5 @@ class atbash_num:
         return "".join([string.ascii_lowercase[::-1][string.ascii_lowercase.index(i)] for i in self.raw])
 
 if __name__ == "__main__":
-    test = atbash_num("coucou")
+    test = atbash_num("test")
     print(test.encrypt())
