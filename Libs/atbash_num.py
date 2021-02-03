d3643acb5b324c45b51c906a34c3369699a4748f
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2021-02-03 16:21:34 +0100

    Add files via upload

---
+++ Libs/atbash_num.py
@@ -0,0 +1,11 @@
+import string
+class atbash_num:
+    def __init__(self,raw):
+        self.raw = raw
+    
+    def encrypt(self):
+        return "".join([string.ascii_lowercase[::-1][string.ascii_lowercase.index(i)] for i in self.raw])
+
+if __name__ == "__main__":
+    test = atbash_num("coucou")
+    print(test.encrypt())
